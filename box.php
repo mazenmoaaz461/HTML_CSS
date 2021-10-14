2dda9af9ea622902a94498819196e91365c4bad6
mazenhazem564 <mazenhazem564@gmail.com>
2021-10-13 17:00:57 -0700

    first commit

---
+++ box.php
@@ -0,0 +1,30 @@
+<html>
+<style>
+.box1{
+  width: 150px;
+  height: 100px;
+  padding: 0.5em;
+  margin: 0.5em;
+  border: 10px solid blue;
+  float: right;
+  }
+  .box2
+  {
+    width:  20px;
+    height: 20px;
+    padding: 0.5em;
+    margin: 0.5em;
+    border: 10px solid red;
+    float: right;
+  }
+  </style>
+
+<body>
+  <div class="box1">
+  Hooray, a box!
+     <div class="box2">
+  </div>
+  </div>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi sit amet mauris commodo. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Volutpat est velit egestas dui id ornare arcu. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue. Porttitor lacus luctus accumsan tortor posuere ac ut consequat. Et malesuada fames ac turpis. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Sed risus ultricies tristique nulla. At auctor urna nunc id. Pharetra convallis posuere morbi leo. Quam quisque id diam vel quam elementum pulvinar etiam. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Amet risus nullam eget felis eget nunc lobortis.</p>
+</body>
+</html>
